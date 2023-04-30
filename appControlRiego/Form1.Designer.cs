0021de9619a8db446f82961dc16e3bfeb398f6d1
Johan Ortiz <m.johan.ortiz@gmail.com>
2023-04-30 10:18:36 -0500

    Proyecto

---
+++ appControlRiego/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace appControlRiego
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código generado por el Diseñador de Windows Forms
+
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.trackBar1 = new System.Windows.Forms.TrackBar();
+			this.trackBar2 = new System.Windows.Forms.TrackBar();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// trackBar1
+			// 
+			this.trackBar1.Location = new System.Drawing.Point(66, 390);
+			this.trackBar1.Maximum = 45;
+			this.trackBar1.Minimum = -10;
+			this.trackBar1.Name = "trackBar1";
+			this.trackBar1.Size = new System.Drawing.Size(164, 56);
+			this.trackBar1.TabIndex = 1;
+			this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+			this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+			// 
+			// trackBar2
+			// 
+			this.trackBar2.Location = new System.Drawing.Point(334, 390);
+			this.trackBar2.Maximum = 100;
+			this.trackBar2.Name = "trackBar2";
+			this.trackBar2.Size = new System.Drawing.Size(164, 56);
+			this.trackBar2.TabIndex = 2;
+			this.trackBar2.TickStyle = System.Windows.Forms.TickStyle.None;
+			this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(76, 438);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(90, 17);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Temperatura";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(341, 438);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(69, 17);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Humedad";
+			// 
+			// panel1
+			// 
+			this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+			this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panel1.Controls.Add(this.label6);
+			this.panel1.Controls.Add(this.label5);
+			this.panel1.Location = new System.Drawing.Point(13, 13);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(535, 360);
+			this.panel1.TabIndex = 5;
+			// 
+			// label6
+			// 
+			this.label6.BackColor = System.Drawing.Color.Transparent;
+			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.label6.Location = new System.Drawing.Point(291, 39);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(243, 43);
+			this.label6.TabIndex = 1;
+			this.label6.Text = "Hora";
+			this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label5
+			// 
+			this.label5.BackColor = System.Drawing.Color.Transparent;
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.label5.Location = new System.Drawing.Point(3, 39);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(243, 43);
+			this.label5.TabIndex = 0;
+			this.label5.Text = "Tiempo";
+			this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(184, 438);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(46, 17);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "label3";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(442, 438);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(46, 17);
+			this.label4.TabIndex = 7;
+			this.label4.Text = "label4";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.Window;
+			this.ClientSize = new System.Drawing.Size(560, 484);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.trackBar2);
+			this.Controls.Add(this.trackBar1);
+			this.Name = "Form1";
+			this.Text = "Control de Riego";
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+			this.panel1.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.TrackBar trackBar1;
+		private System.Windows.Forms.TrackBar trackBar2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label5;
+	}
+}
+
---
+++ appControlRiego/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Fuzzy;
+
+
+namespace appControlRiego
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			//AForge.Fuzzy
+			//InferenceSystem fis = new InferenceSystem(new Database(),new Aforge. InferenceSystemSerializer().Deserialize("ruta/al/archivo.fis"));
+			InitializeComponent();
+			this.label3.Text = this.trackBar1.Value.ToString();
+			this.label4.Text = this.trackBar2.Value.ToString();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void trackBar1_Scroll(object sender, EventArgs e)
+		{
+			this.label3.Text = this.trackBar1.Value.ToString();
+		}
+
+		private void trackBar2_Scroll(object sender, EventArgs e)
+		{
+			this.label4.Text = this.trackBar2.Value.ToString();
+		}
+	}
+}
